74206bd31b9d784151756d7c7d037298f85f814b
OliverDanger <od.oliverdixon@gmail.com>
2023-03-11 20:35:27 +0000

    made recursive sum function

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 
+
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
+
 }
+
+// Make sum a recursive function that sums all the whole numbers from fromN to toN.
+
+// So if we called the function with the following values: sum(3, 7), it would sum 3 + 4 + 5 + 6 + 7 and output 25.
+
+// For this exercise, we can safely assume that fromN will always be less than or equal to toN.
+
+
+//testing
+
+console.log('returned:',sum(2, 20));
 
 module.exports = sum;
